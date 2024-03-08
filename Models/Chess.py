e5141170362576cf4fd256a6e113aab435aabb7a
francois gourbal <gourbal.francois@gmail.com>
2024-03-08 12:05:28 +0100

    img, and piece link with img

--- Models/Chess.py
+++ Models/Chess.py
@@ -1,6 +1,22 @@
 import tkinter as tk
 
 
+class ChessSquare(tk.Canvas):
+    def __init__(self, master, color, piece=None, **kwargs):
+        super().__init__(master, width=100, height=93, bg=color, **kwargs)
+        self.piece = piece
+
+        if self.piece:
+            self.add_piece()
+
+    def add_piece(self):
+        color = "BLACK" if self.piece < 0 else "WHITE"
+        piece_type = abs(self.piece)
+        image_path = f"images/{color}/{color.lower()}_{'pawn' if piece_type == 1 else 'king' if piece_type == 6 else 'queen' if piece_type == 5 else 'bishop' if piece_type == 3 else 'horse' if piece_type == 4 else 'tower'}.png"
+        self.image = tk.PhotoImage(file=image_path)
+        self.create_image(50, 50, image=self.image)
+
+
 class ChessBoard(tk.Frame):
     chessboard = [
         [-2, -3, -4, -5, -6, -4, -3, -2],
@@ -14,46 +30,14 @@ class ChessBoard(tk.Frame):
     ]
 
     def __init__(self, root):
-        super().__init__()
+        super().__init__(root)
         self.root = root
-        self.canvas = tk.Canvas(root, width=400, height=400)
-        self.canvas.pack()
-        self.draw_board_cases()
+        self.create_board()
 
-    def draw_board_cases(self):
+    def create_board(self):
         for i in range(8):
             for j in range(8):
-                if (i + j) % 2 == 0:
-                    self.canvas.create_rectangle(i * 50, j * 50, (i + 1) * 50, (j + 1) * 50, fill="white")
-                else:
-                    self.canvas.create_rectangle(i * 50, j * 50, (i + 1) * 50, (j + 1) * 50, fill="black")
-
-    def link_pieces_to_board(self):
-        for i in range(8):
-            for j in range(8):
-                if self.chessboard[i][j] == -2:
-                    self.canvas.create_image(i * 50, j * 50, image=self.black_tower)
-                elif self.chessboard[i][j] == -3:
-                    self.canvas.create_image(i * 50, j * 50, image=self.black_bishop)
-                elif self.chessboard[i][j] == -4:
-                    self.canvas.create_image(i * 50, j * 50, image=self.black_horse)
-                elif self.chessboard[i][j] == -5:
-                    self.canvas.create_image(i * 50, j * 50, image=self.black_queen)
-                elif self.chessboard[i][j] == -6:
-                    self.canvas.create_image(i * 50, j * 50, image=self.black_king)
-                elif self.chessboard[i][j] == -1:
-                    self.canvas.create_image(i * 50, j * 50, image=self.black_pawn)
-                elif self.chessboard[i][j] == 1:
-                    self.canvas.create_image(i * 50, j * 50, image=self.white_pawn)
-                elif self.chessboard[i][j] == 2:
-                    self.canvas.create_image(i * 50, j * 50, image=self.white_tower)
-                elif self.chessboard[i][j] == 3:
-                    self.canvas.create_image(i * 50, j * 50, image=self.white_bishop)
-                elif self.chessboard[i][j] == 4:
-                    self.canvas.create_image(i * 50, j * 50, image=self.white_horse)
-                elif self.chessboard[i][j] == 5:
-                    self.canvas.create_image(i * 50, j * 50, image=self.white_queen)
-                elif self.chessboard[i][j] == 6:
-                    self.canvas.create_image(i * 50, j * 50, image=self.white_king)
-                else:
-                    pass
+                color = "white" if (i + j) % 2 == 0 else "black"
+                piece = self.chessboard[j][i]
+                square = ChessSquare(self.root, color, piece, highlightthickness=0)
+                square.grid(row=j, column=i, padx=1, pady=1)
