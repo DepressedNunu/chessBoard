5f3e431fd6c73ba1060089fe34edd7dab21ca041
francois gourbal <gourbal.francois@gmail.com>
2024-03-11 08:44:32 +0100

    revert to previous version

--- Models/Chess.py
+++ Models/Chess.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-import numpy
-from Models.piece import *
 
 
 class ChessSquare(tk.Canvas):
@@ -11,36 +9,28 @@ class ChessSquare(tk.Canvas):
         if self.piece:
             self.add_piece()
 
-    def add_piece(self):
-        color = "BLACK" if self.piece < 0 else "WHITE"
-        piece_type = abs(self.piece)
-        image_path = f"images/{color}/{color.lower()}_{'pawn' if piece_type == 1 else 'king' if piece_type == 6 else 'queen' if piece_type == 5 else 'bishop' if piece_type == 3 else 'horse' if piece_type == 4 else 'tower'}.png"
-        self.image = tk.PhotoImage(file=image_path)
+    def add_piece(self, piece: Piece):
+        self.image = tk.PhotoImage(file=piece.pathList)
         self.create_image(50, 50, image=self.image)
 
 
 class ChessBoard(tk.Frame):
-    chessData = numpy.array([[0 for i in range(8)] for j in range(8)])
+    chessboard = [
+        [-2, -3, -4, -5, -6, -4, -3, -2],
+        [-1, -1, -1, -1, -1, -1, -1, -1],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 3, 4, 5, 6, 4, 3, 2],
+    ]
 
-    def place_piece(self, piece: Piece):
-        self.chessData[piece.position[1]][piece.position[0]] = piece.pieceType.value
     def __init__(self, root):
         super().__init__(root)
         self.root = root
         self.create_board()
 
-    def move_piece(self, piece: Piece, new_position: tuple):
-        if self.validate_move(piece, new_position):
-            self.chessData[piece.position[1]][piece.position[0]] = 0
-            piece.move(new_position)
-            self.chessData[new_position[1]][new_position[0]] = piece.pieceType.value
-
-    def validate_move(self, piece: Piece, new_position: tuple):
-        return True
-
-    def display_board(self):
-        for row in self.chessData:
-            print(row)
     def create_board(self):
         for i in range(8):
             for j in range(8):
--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from Models import *
+from Models import Chess
 import tkinter as tk
 
-from Models.Chess import ChessBoard
-from Models.piece import Piece
-from Models.piece import PieceType
-
 if __name__ == '__main__':
-    board = ChessBoard()
-    board.display_board()
-    print("\n===========================================\n")
-    pawn = Piece(PieceType.pawnBlack, (1, 1))
-    print(pawn.pieceType.value)
-    print(pawn.path)
+    root = tk.Tk()
+    app = Chess.ChessBoard(root)
+    app.mainloop()
