4f7ea7add6199cd5e32c764e84d9c4adeae428f6
aflorian <florian.albora@ynov.com>
2024-03-17 23:14:33 +0100

    feat : add highlighting

--- src_view/board.py
+++ src_view/board.py
@@ -15,41 +15,68 @@ def set_color(position_x: int, position_y: int) -> str:
 
 
 class SquareCanvas(tk.Canvas):
-    def __init__(self, master, chess_square: ChessSquare):
+    def __init__(self, master, chess_square: ChessSquare, click_callback):
         super().__init__(master, width=100, height=100, background=set_color(chess_square.x, chess_square.y))
+        self.chess_square = chess_square
         self.position_x = chess_square.x
         self.position_y = chess_square.y
+        self.click_callback = click_callback
         if chess_square.has_value:
             self.display_piece(chess_square.piece)
-
-        self.create_text(80, 10, text=f'{self.position_x},{self.position_y}', fill='black')
+        # Add coordinates text
+        self.create_text(50, 10, text=f'{self.position_x},{self.position_y}', fill='black')
+        # Listen click
+        self.bind("<Button-1>", self.on_square_clicked)
 
     def display_piece(self, piece: Piece):
         image = Image.open(piece.path)
-        image = image.resize((80, 80), Image.Resampling.LANCZOS)
+        image = image.resize((70, 70), Image.Resampling.LANCZOS)
         self.piece_image = ImageTk.PhotoImage(image)
         self.create_image(50, 50, image=self.piece_image)
 
+    def on_square_clicked(self, event):
+        self.click_callback(self)
+
 
 class BoardCanvas(tk.Canvas):
-    def __init__(self, master, h, w):
+    def __init__(self, master, h, w, click_callback):
         super().__init__(master)
         self.configure(width=w, height=h)
         self.grid(row=0, column=0)
+        self.click_callback = click_callback
 
-    def add_square(self, chess_square: ChessSquare):
-        square = SquareCanvas(self, chess_square)
+    def add_square(self, chess_square: ChessSquare, click_callback):
+        square = SquareCanvas(self, chess_square, click_callback)
         square.grid(row=chess_square.y, column=chess_square.x)
 
 
 class BoardWindow(tk.Tk):
     def __init__(self, chess_board: ChessBoard):
         super().__init__()
+
+        # Variables
+        self.is_highlighted = False
+        self.possible_moves_list = None
+
+        # Move variables
+        self.new_position = None
+        self.last_selected_piece = None
+
         self.title("Chess Board")
-        self.board = BoardCanvas(self, 800, 800)
+        self.board = BoardCanvas(self, 800, 800, self.handle_square_click)
         self.create_board(chess_board)
 
     def create_board(self, board: ChessBoard):
         for i in range(8):
             for j in range(8):
-                self.board.add_square(board.board[i][j])
+                self.board.add_square(board.board[i][j], self.handle_square_click)
+
+    def handle_square_click(self, square_canvas):
+        if self.last_selected_piece and self.last_selected_piece is not square_canvas:
+            # Unselect the last square
+            self.last_selected_piece.config(
+                background=set_color(self.last_selected_piece.position_x, self.last_selected_piece.position_y))
+        # Select the new
+        if square_canvas.chess_square.has_value:
+            square_canvas.config(background=highlight_square)
+            self.last_selected_piece = square_canvas
