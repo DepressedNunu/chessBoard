49ba48e126e9f422fd6bc5379eabe5b9ea2b22b1
francois gourbal <gourbal.francois@gmail.com>
2024-04-02 22:48:13 +0200

    merge entre main vers interface

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.Models.chess_board import ChessBoard
 from src.display.board import BoardWindow
+
 if __name__ == "__main__":
     chess_board = ChessBoard()
     app = BoardWindow(chess_board)
