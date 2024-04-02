d626e678dc285ca85da46891b4d2ac78c59db3a2
aflorian <florian.albora@ynov.com>
2024-04-02 22:40:18 +0200

    tentative d'ajout des focntions basiques d'ia

--- src/Models/ia.py
+++ src/Models/ia.py
@@ -1,14 +1,26 @@
 import random
 
+from src.Models.moves import Move
+from src.Models.piece import Position
+
 
 class ia:
     def __init__(self, board):
         self.board = board
 
     def play(self, board, piece_list):
-        piece, move = self.get_move(board, piece_list)
-        print(f"IA move: {piece.pieceType} to {move}")
-        self.board.move(piece, move[::-1])
+        piece, destination = self.get_move(board, piece_list)
+
+        move = Move(
+            initial_position=Position(piece.position.x, piece.position.y),
+            move_position=Position(destination[0], destination[1]),
+            piece=piece,
+            captured_piece=board.board[destination[1]][destination[0]].piece)
+
+        print(board.game_moves.add_move(move, board.is_check(self.board.turn),
+                                        board.is_checkmate(board.turn)))
+
+        self.board.move(piece, destination[::-1])
 
     def get_move(self, board, piece_list):
         # without CSV
@@ -22,9 +34,8 @@ class ia:
         list_move = None
         while move is None or list_move is None:
             piece = random.choice(piece_list)
-            list_move = board.get_possible_moves(piece)
-            if list_move is [] or list_move is None:
-                piece = None
+            list_move = board.filter_possible_moves(piece)
+            if len(list_move) == 0 or list_move is None:
                 continue
             move = list_move[0]
             move = (move[1], move[0])
--- src/Models/moves.py
+++ src/Models/moves.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
-from src.Models.piece import Position, Piece
+import pandas as pd
+from src.Models.piece import Position, Piece, PieceType
 
 
 class Algebraic_piece_name(Enum):
@@ -28,18 +29,82 @@ class Move:
         self.move_position = move_position
         self.color = piece.color
         self.piece = piece
-        self.score = score
         self.captured_piece = captured_piece
 
-    def to_algebraic_notation(self):
+    def to_algebraic_notation(self, is_check: bool, is_checkmate: bool) -> str:
         piece_letter = Algebraic_piece_name[self.piece.pieceType.name].value
         capture = ""
         if self.captured_piece is not None:
-            if piece_letter == "":
-                capture = chr(ord('a') + self.initial_position.y) + "x"
-            else:
-                capture = "x"
+            capture = "x"
+
+        check_mate = ""
+        check = ""
+        if is_checkmate:
+            check_mate = "#"
+            check = ""
 
         self.algebraic_notation = piece_letter + capture + chr(ord('a') + self.move_position.y) + str(
-            8 - self.move_position.x)
+            8 - self.move_position.x) + check_mate + check
         return self.algebraic_notation
+
+    def calculate_score(self) -> int:
+        if self.captured_piece is not None:
+            if self.captured_piece.pieceType == PieceType.PAWN:
+                return 1
+            elif self.captured_piece.pieceType == PieceType.ROOK:
+                return 5
+            elif self.captured_piece.pieceType == PieceType.QUEEN:
+                return 9
+            elif self.captured_piece.pieceType == PieceType.BISHOP or self.captured_piece.pieceType == PieceType.KNIGHT:
+                return 3
+        else:
+            return 0
+
+
+class GameMoves:
+    def __init__(self):
+        self.move_df = pd.DataFrame(columns=['White', 'Black', 'White_move_score', 'Black_move_score'], index=['Turns'])
+        self.df_import = pd.read_csv('saves/games_saves.csv', sep=';', index_col=['Game', 'Moves'])
+
+    def get_df_with_winner(self, move: Move):
+        game_index = self.df_import.index.levels[0].values[-1]+1
+
+        winner_df = pd.DataFrame({
+            'White': ["winner" if move.color else "loser"],
+            'Black': ["winner" if not move.color else "loser"]
+        })
+
+        self.move_df = pd.concat([self.move_df, winner_df])
+        self.move_df.index = pd.MultiIndex.from_product([[game_index], self.move_df.index], names=['Game', 'Moves'])
+        return self.move_df
+
+    def insert_to_scv(self, move: Move):
+        self.df_import = pd.concat([self.df_import, self.get_df_with_winner(move)])
+        self.df_import.to_csv('saves/games_saves.csv', index=True, header=True, sep=';')
+
+    def add_move(self, move: Move, is_check: bool, is_checkmate: bool):
+        score = move.calculate_score()
+        if is_checkmate:
+            is_check = False
+            score += 20
+        if is_check:
+            score += 10
+            color = 'Black' if move.color else 'White'
+            last_opponent_move = self.move_df.at[self.move_df.index[-1], color]
+            last_opponent_move_score = self.move_df.at[self.move_df.index[-1], color + "_move_score"]
+            self.move_df.at[self.move_df.index[-1], color] = last_opponent_move + "+"
+            self.move_df.at[self.move_df.index[-1], color + "_move_score"] = last_opponent_move_score + score
+
+        if move.color:
+            # if White
+            new_row = [move.to_algebraic_notation(is_check, is_checkmate), "None", move.calculate_score(), None]
+            if is_checkmate:
+                new_row[2] += 20
+                self.move_df.loc[len(self.move_df) - 1] = new_row
+            else:
+                self.move_df.loc[len(self.move_df)] = new_row
+
+        else:
+            self.move_df.at[self.move_df.index[-1], 'Black'] = move.to_algebraic_notation(is_check, is_checkmate)
+            self.move_df.at[self.move_df.index[-1], 'Black_move_score'] = score
+        return self.move_df
